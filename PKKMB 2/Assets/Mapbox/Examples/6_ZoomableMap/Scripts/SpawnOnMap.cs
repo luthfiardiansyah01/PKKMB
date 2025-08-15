3241f1274ef9f66d4b768fbc3330e8502ce54ce6
Naufalsh <naufalmaulanaas@gmail.com>
2025-08-15 14:10:57 +0700

    Membuat Script dan assign building

--- PKKMB 2/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ PKKMB 2/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -1,55 +1,79 @@
-﻿namespace Mapbox.Examples
+﻿using UnityEngine;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using System.Collections.Generic;
+
+namespace Mapbox.Examples
 {
-	using UnityEngine;
-	using Mapbox.Utils;
-	using Mapbox.Unity.Map;
-	using Mapbox.Unity.MeshGeneration.Factories;
-	using Mapbox.Unity.Utilities;
-	using System.Collections.Generic;
+    // KELAS BARU UNTUK MEMASANGKAN DATA (bisa ditaruh di sini atau di file sendiri)
+    [System.Serializable]
+    public class Placement
+    {
+        public string BuildingId;
+        public GameObject BuildingPrefab;
+        [Geocode]
+        public string LocationString;
 
-	public class SpawnOnMap : MonoBehaviour
-	{
-		[SerializeField]
-		AbstractMap _map;
+    }
 
-		[SerializeField]
-		[Geocode]
-		string[] _locationStrings;
-		Vector2d[] _locations;
+    public class SpawnOnMap : MonoBehaviour
+    {
+        [SerializeField]
+        AbstractMap _map;
 
-		[SerializeField]
-		float _spawnScale = 100f;
+        [SerializeField]
+        Placement[] _placements;
 
-		[SerializeField]
-		GameObject _markerPrefab;
+        [SerializeField]
+        float _spawnScale = 100f;
 
-		List<GameObject> _spawnedObjects;
+        List<GameObject> _spawnedObjects;
+        Vector2d[] _locations;
 
-		void Start()
-		{
-			_locations = new Vector2d[_locationStrings.Length];
-			_spawnedObjects = new List<GameObject>();
-			for (int i = 0; i < _locationStrings.Length; i++)
-			{
-				var locationString = _locationStrings[i];
-				_locations[i] = Conversions.StringToLatLon(locationString);
-				var instance = Instantiate(_markerPrefab);
-				instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
-				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-				_spawnedObjects.Add(instance);
-			}
-		}
+        void Start()
+        {
+            _spawnedObjects = new List<GameObject>();
+            _locations = new Vector2d[_placements.Length];
 
-		private void Update()
-		{
-			int count = _spawnedObjects.Count;
-			for (int i = 0; i < count; i++)
-			{
-				var spawnedObject = _spawnedObjects[i];
-				var location = _locations[i];
-				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-			}
-		}
-	}
+            for (int i = 0; i < _placements.Length; i++)
+            {
+                var placement = _placements[i];
+
+                // Konversi string lokasi menjadi koordinat Vector2d
+                _locations[i] = Conversions.StringToLatLon(placement.LocationString);
+
+                // Instantiate prefab SPESIFIK untuk placement ini
+                var instance = Instantiate(placement.BuildingPrefab); // -> Menggunakan prefab dari data placement
+                _spawnedObjects.Add(instance); // Tambahkan ke daftar untuk di-update
+
+                BuildingTrigger triggerScript = instance.GetComponent<BuildingTrigger>();
+                if (triggerScript != null)
+                {
+                    triggerScript.buildingId = placement.BuildingId;
+                }
+                else
+                {
+                    Debug.LogWarning("Prefab " + placement.BuildingPrefab.name + " tidak memiliki komponen BuildingTrigger!");
+                }
+
+                // Atur posisi dan skala seperti sebelumnya
+                instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+                instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+            }
+        }
+
+        private void Update()
+        {
+
+            int count = _spawnedObjects.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var spawnedObject = _spawnedObjects[i];
+                var location = _locations[i];
+                spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+                spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+            }
+        }
+    }
 }
