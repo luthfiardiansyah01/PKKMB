d02cdccf6280e495c5d7b7f16ec006f9b4ceb863
Naufalsh <naufalmaulanaas@gmail.com>
2025-09-02 10:56:24 +0700

    BuildingCount Function

--- PKKMB 2/Assets/Script/BuildingTrigger.cs
+++ PKKMB 2/Assets/Script/BuildingTrigger.cs
@@ -1,9 +1,12 @@
+using PlayFab;
+using PlayFab.ClientModels;
 using UnityEngine;
 
 public class BuildingTrigger : MonoBehaviour
 {
     [SerializeField] public string buildingId;
     private GameObject questionMark;
+    private string namaGedung;
 
     // public QuestionMarkManager questionMarkManager;
 
@@ -11,8 +14,9 @@ public class BuildingTrigger : MonoBehaviour
 
     void Start()
     {
+        GetNameBuilding();
         // Cari child bernama "QuestionMark" dari building ini
-       
+
         Transform qmTransform = transform.Find("QuestionMark");
         if (qmTransform != null)
         {
@@ -27,7 +31,7 @@ public class BuildingTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+
         if (!other.CompareTag("Player")) return;
 
         // Aktifkan question mark
@@ -35,16 +39,18 @@ public class BuildingTrigger : MonoBehaviour
         {
             questionMark.SetActive(true);
             Debug.Log($"Question mark untuk {buildingId} ditampilkan");
-            
+
             if (QuestionMarkManager.Instance != null)
             {
                 QuestionMarkManager.Instance.currentBuildingId = buildingId;
                 // CheckQuizStatus(); //ini bikin error
             }
+            UpdateBuildingVisit(namaGedung);
         }
+
     }
 
-     private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         if (!other.CompareTag("Player")) return;
 
@@ -55,4 +61,35 @@ public class BuildingTrigger : MonoBehaviour
             Debug.Log($"Question mark untuk {buildingId} ditutup");
         }
     }
+
+    public void UpdateBuildingVisit(string buildingId)
+    {
+        var request = new ExecuteCloudScriptRequest
+        {
+            FunctionName = "UpdateBuildingVisit", // nama handler yang ada di CloudScript
+            FunctionParameter = new { buildingId = buildingId }
+        };
+
+        PlayFabClientAPI.ExecuteCloudScript(request, result =>
+        {
+            Debug.Log("Success: " + result.FunctionResult);
+        },
+        error =>
+        {
+            Debug.LogError("Error: " + error.GenerateErrorReport());
+        });
+    }
+
+    private void GetNameBuilding()
+    {
+        if (GameManager.Instance != null && GameManager.Instance.buildingCache.ContainsKey(buildingId))
+        {
+            BuildingData targetBuilding = GameManager.Instance.buildingCache[buildingId];
+
+            // Isi panel Info sesuai gedung
+            namaGedung = targetBuilding.name;
+           
+        }
+    }
+
 }
